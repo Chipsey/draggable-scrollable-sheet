6b198ebf4622772362a402675c1afe180ff02fa6
Udana D. Kodikara <chipseyofficial@gmail.com>
2024-06-14 22:11:04 +0530

    Usability enhanced

--- draggable-bottom-sheet.dart
+++ draggable-bottom-sheet.dart
@@ -4,38 +4,42 @@ import 'package:flutter/material.dart';
 
 class CustomDraggableBottomSheet extends StatelessWidget {
   final Widget child;
+  final double size;
 
-  const CustomDraggableBottomSheet({super.key, required this.child});
+  const CustomDraggableBottomSheet(
+      {super.key, required this.child, required this.size});
 
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
       initialChildSize: 0.5,
       minChildSize: 0.3,
-      maxChildSize: 1.0,
+      maxChildSize: 0.93,
       expand: false,
       builder: (context, scrollController) {
         return Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: const BoxDecoration(
+          padding: const EdgeInsets.symmetric(vertical: 0),
+          decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(size)),
           ),
           child: SingleChildScrollView(
             controller: scrollController,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            child: Stack(
+              // mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Container(
-                  width: 40,
-                  height: 4,
-                  margin: const EdgeInsets.symmetric(vertical: 8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(2.0),
+                child,
+                Center(
+                  child: Container(
+                    width: size * 2,
+                    height: size * 0.3,
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(size),
+                    ),
                   ),
                 ),
-                child,
               ],
             ),
           ),
